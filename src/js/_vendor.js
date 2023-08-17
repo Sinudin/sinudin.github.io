366f2e09dcb5ed190b64b8380cb03ef76090363d
Andrey Nefyodov <canionsoulman@gmail.com>
2023-08-17 10:28:21 +0300

    mainx

--- src/js/_vendor.js
+++
@@ -1,3 +0,0 @@
-import './vendor/focus-visible.js';
-import './vendor/itc-slider.js';
-
--- src/js/vendor/app.js
+++
@@ -1,8 +0,0 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-
-ScrollSmoother.create({
-  wrapper: '.bob',
-  content: '.site-container',
-  smooth: 1.5,
-  effects: true
-})
